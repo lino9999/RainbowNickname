33cdbe18a3ec5196574a932d5be436aa25154714
Lino <lino9999.123@gmail.com>
2025-11-23 21:34:27 +0100

    c

--- src/main/java/com/Lino/rainbowNickname/RainbowNickname.java
+++ src/main/java/com/Lino/rainbowNickname/RainbowNickname.java
@@ -1,240 +1,113 @@
 package com.Lino.rainbowNickname;
 
-import com.Lino.rainbowNickname.data.PlayerData;
-import com.Lino.rainbowNickname.handlers.CommandHandler;
-import com.Lino.rainbowNickname.handlers.EventListener;
-import com.Lino.rainbowNickname.managers.*;
+import com.Lino.rainbowNickname.commands.MainCommand;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scoreboard.Team;
+import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.UUID;
+public class RainbowNickname extends JavaPlugin implements Listener {
 
-public class RainbowNickname extends JavaPlugin {
-    private static final String PERMISSION = "rainbownick.use";
-
-    private ConfigManager configManager;
-    private AnimationManager animationManager;
-    private ArmorStandManager armorStandManager;
-    private TabListManager tabListManager;
-    private PlayerDataManager playerDataManager;
-    private TaskManager taskManager;
-    private TPSMonitor tpsMonitor;
+    private NickManager nickManager;
+    private MessageManager messageManager;
+    private DataManager dataManager;
+    private HookManager hookManager;
+    private BukkitRunnable animationTask;
+    private float rainbowPhase = 0.0f;
 
     @Override
     public void onEnable() {
-        configManager = new ConfigManager(this);
-        animationManager = new AnimationManager(configManager);
-        armorStandManager = new ArmorStandManager(configManager, animationManager);
-        tabListManager = new TabListManager(configManager, animationManager);
-        playerDataManager = new PlayerDataManager(configManager, animationManager, tabListManager);
-        taskManager = new TaskManager(this, configManager, playerDataManager, tabListManager, armorStandManager);
-        tpsMonitor = new TPSMonitor(this, configManager);
+        this.messageManager = new MessageManager(this);
+        this.dataManager = new DataManager(this);
+        this.hookManager = new HookManager();
+        this.nickManager = new NickManager(this);
 
-        taskManager.setTPSMonitor(tpsMonitor);
+        getServer().getPluginManager().registerEvents(this, this);
+        getCommand("rainbownick").setExecutor(new MainCommand(this));
 
-        getServer().getPluginManager().registerEvents(new EventListener(this), this);
-        getCommand("rainbownick").setExecutor(new CommandHandler(this));
-
-        tabListManager.cleanupOldTeams();
-        armorStandManager.cleanupOrphanedArmorStands();
-
-        tpsMonitor.start();
-
-        if (configManager.isUseAsyncTasks()) {
-            Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
-                for (Player player : Bukkit.getOnlinePlayers()) {
-                    if (player.hasPermission(PERMISSION)) {
-                        animationManager.precachePlayerNames(player.getName());
-                    }
-                }
-
-                Bukkit.getScheduler().runTask(this, () -> {
-                    for (Player player : Bukkit.getOnlinePlayers()) {
-                        if (player.hasPermission(PERMISSION)) {
-                            setupPlayer(player);
-                        } else if (configManager.isKeepPrefixSuffix()) {
-                            tabListManager.forceUpdateNonRainbowPlayer(player, player.getName());
-                        }
-                    }
-                    taskManager.startAllTasks();
-                });
-            });
-        } else {
-            Bukkit.getScheduler().runTaskLater(this, () -> {
-                for (Player player : Bukkit.getOnlinePlayers()) {
-                    if (player.hasPermission(PERMISSION)) {
-                        setupPlayer(player);
-                    } else if (configManager.isKeepPrefixSuffix()) {
-                        tabListManager.forceUpdateNonRainbowPlayer(player, player.getName());
-                    }
-                }
-                taskManager.startAllTasks();
-            }, 20L);
+        if (Bukkit.getPluginManager().isPluginEnabled("LuckPerms")) {
+            getLogger().info("LuckPerms rilevato! Prefissi abilitati.");
         }
 
-        getLogger().info("RainbowNickname enabled - Version with prefix/suffix support!");
-        getLogger().info("Prefix/Suffix: " + (configManager.isKeepPrefixSuffix() ? "ENABLED for ALL players" : "DISABLED"));
-        getLogger().info("Read from tab: " + (configManager.isReadFromTab() ? "ENABLED" : "DISABLED"));
-        getLogger().info("Delay LuckPerms: " + configManager.getLuckpermsJoinDelay() + " ticks");
-        getLogger().info("TPS Auto-adjust: " + (configManager.isAutoAdjustSpeed() ? "ENABLED" : "DISABLED"));
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (dataManager.isNickEnabled(player.getUniqueId())) {
+                nickManager.enableNick(player);
+            }
+        }
+
+        startAnimationLoop();
+        getLogger().info("RainbowNickname v2.1 abilitato!");
     }
 
     @Override
     public void onDisable() {
-        taskManager.cancelAllTasks();
-        tpsMonitor.stop();
-
-        playerDataManager.getAllPlayerData().values().parallelStream().forEach(data -> {
-            if (data.armorStand != null && !data.armorStand.isDead()) {
-                Bukkit.getScheduler().runTask(this, () -> data.armorStand.remove());
-            }
-        });
-
-        armorStandManager.cleanupOrphanedArmorStands();
-
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            restorePlayer(player);
-            if (configManager.isUseTabList()) {
-                player.setPlayerListHeader("");
-                player.setPlayerListFooter("");
-            }
+        if (animationTask != null && !animationTask.isCancelled()) {
+            animationTask.cancel();
         }
-
-        tabListManager.cleanupOldTeams();
-        playerDataManager.clearAll();
-        animationManager.clearCache();
-        armorStandManager.clearArmorStands();
-        tabListManager.clearCaches();
-
-        getLogger().info("RainbowNickname disabled!");
+        nickManager.removeAll();
+        getLogger().info("RainbowNickname disabilitato.");
     }
 
-    public void setupPlayer(Player player) {
-        UUID uuid = player.getUniqueId();
-        String name = player.getName();
+    public void reloadPlugin() {
+        onDisable();
+        onEnable();
+        messageManager.loadMessages();
+        dataManager.loadData();
+    }
 
-        PlayerData oldData = playerDataManager.getPlayerData(uuid);
-        if (oldData != null && oldData.armorStand != null && !oldData.armorStand.isDead()) {
-            armorStandManager.removeArmorStand(oldData.armorStand);
-        }
+    private void startAnimationLoop() {
+        animationTask = new BukkitRunnable() {
+            @Override
+            public void run() {
+                rainbowPhase -= 0.05f;
+                if (rainbowPhase < -100.0f) rainbowPhase = 0.0f;
 
-        if (!animationManager.hasCachedNames(name)) {
-            animationManager.precachePlayerNames(name);
-        }
-
-        Team luckPermsTeam = null;
-        String savedPrefix = "";
-        String savedSuffix = "";
-
-        for (Team team : tabListManager.getScoreboard().getTeams()) {
-            if (!team.getName().startsWith("rn_") && team.hasEntry(player.getName())) {
-                luckPermsTeam = team;
-                savedPrefix = team.getPrefix();
-                savedSuffix = team.getSuffix();
-                break;
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    if (nickManager.hasNick(player)) {
+                        try {
+                            nickManager.updateNick(player, rainbowPhase);
+                        } catch (Exception ignored) {}
+                    }
+                }
             }
-        }
+        };
+        animationTask.runTaskTimer(this, 1L, 1L);
+    }
 
-        tabListManager.hideOriginalNametag(player);
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        Bukkit.getScheduler().runTaskLater(this, () -> {
+            if (player.isOnline() && dataManager.isNickEnabled(player.getUniqueId())) {
+                nickManager.enableNick(player);
+            }
+        }, 20L);
+    }
 
-        ArmorStand armorStand = armorStandManager.createArmorStand(player);
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        nickManager.disableNick(event.getPlayer());
+    }
 
-        PlayerData data = playerDataManager.createPlayerData(player, armorStand);
-        data.cachedPrefix = savedPrefix;
-        data.cachedSuffix = savedSuffix;
-        data.luckPermsTeam = luckPermsTeam;
-
-        playerDataManager.addPlayerData(uuid, data);
-
-        playerDataManager.updatePlayerColor(uuid);
-
-        if (configManager.isUseTabList()) {
-            tabListManager.updateTabList(player);
+    @EventHandler
+    public void onTeleport(PlayerTeleportEvent event) {
+        Player player = event.getPlayer();
+        if (nickManager.hasNick(player)) {
+            Bukkit.getScheduler().runTaskLater(this, () -> {
+                if (player.isOnline()) {
+                    nickManager.refreshPassenger(player);
+                }
+            }, 5L);
         }
     }
 
-    public void cleanupPlayer(UUID uuid) {
-        PlayerData data = playerDataManager.removePlayerData(uuid);
-
-        if (data != null) {
-            if (data.armorStand != null && !data.armorStand.isDead()) {
-                armorStandManager.removeArmorStand(data.armorStand);
-            }
-
-            tabListManager.removePlayerTeam(uuid);
-        }
-
-        tabListManager.removePlayerData(uuid);
-    }
-
-    private void restorePlayer(Player player) {
-        UUID uuid = player.getUniqueId();
-        PlayerData data = playerDataManager.getPlayerData(uuid);
-
-        if (data != null) {
-            player.setDisplayName(data.originalName);
-            if (data.originalListName != null) {
-                player.setPlayerListName(data.originalListName);
-            } else {
-                player.setPlayerListName(data.originalName);
-            }
-        }
-    }
-
-    public void reloadPlugin(CommandSender sender) {
-        sender.sendMessage(ChatColor.YELLOW + "Reloading RainbowNickname...");
-
-        taskManager.cancelAllTasks();
-        tpsMonitor.stop();
-
-        for (PlayerData data : playerDataManager.getAllPlayerData().values()) {
-            if (data.armorStand != null && !data.armorStand.isDead()) {
-                data.armorStand.remove();
-            }
-        }
-
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            restorePlayer(player);
-            if (configManager.isUseTabList()) {
-                player.setPlayerListHeader("");
-                player.setPlayerListFooter("");
-            }
-        }
-
-        playerDataManager.clearAll();
-        animationManager.clearCache();
-        armorStandManager.clearArmorStands();
-        tabListManager.clearCaches();
-
-        configManager.reload();
-
-        tpsMonitor = new TPSMonitor(this, configManager);
-        taskManager.setTPSMonitor(tpsMonitor);
-        tpsMonitor.start();
-
-        armorStandManager.cleanupOrphanedArmorStands();
-
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            if (player.hasPermission(PERMISSION)) {
-                animationManager.precachePlayerNames(player.getName());
-                setupPlayer(player);
-            }
-        }
-
-        taskManager.startAllTasks();
-
-        sender.sendMessage(ChatColor.GREEN + "RainbowNickname reloaded successfully!");
-    }
-
-    public ConfigManager getConfigManager() { return configManager; }
-    public AnimationManager getAnimationManager() { return animationManager; }
-    public ArmorStandManager getArmorStandManager() { return armorStandManager; }
-    public TabListManager getTabListManager() { return tabListManager; }
-    public PlayerDataManager getPlayerDataManager() { return playerDataManager; }
-    public TPSMonitor getTPSMonitor() { return tpsMonitor; }
+    public NickManager getNickManager() { return nickManager; }
+    public MessageManager getMessageManager() { return messageManager; }
+    public DataManager getDataManager() { return dataManager; }
+    public HookManager getHookManager() { return hookManager; }
 }
